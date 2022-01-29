270093033b4a7c32dd6ebf3bce0e58d77b1394e8
Guasonwrg <88564173+Guasonwrg@users.noreply.github.com>
2022-01-28 22:06:40 -0300

    Update usersController.js

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -6,18 +6,13 @@ const fs = require('fs');
 
 const User = require('../models/User');
 
-
+const users = path.join(__dirname, '../dataBase/users.json');
 const usuarios = JSON.parse(fs.readFileSync(users, 'utf-8'));
 
 
 
 
 const userController = {
-
-    /*list: (req, res) => {
-
-        res.render('users/userList', {usuarios});
-    },*/
 
     profile: (req, res) =>{
           return  res.render('users/userProfile', {
@@ -31,7 +26,7 @@ const userController = {
         res.render('users/register');
     },
     proccesRegister: (req, res) =>{
-     /* const resultaValidation =  validationResult(req);
+       /* const resultaValidation =  validationResult(req);
         if (resultaValidation.errors.length > 0){
             res.render('users/register', {
                 errors: resultaValidation.mapped(),
@@ -40,14 +35,15 @@ const userController = {
         };*/
         let userInDB = User.findByField('email', req.body.email);
         if(userInDB){
-          return  res.render('users/register', {
+            res.render('users/register', {
                 errors: {
                     email: {
                         msg: 'Este email ya está registrado'
                     }
                 },
+                oldData: req.body,
             });
-        };
+        }
         const userCreate = {
             nombre: req.body.nombre,
             apellido: req.body.apellido,
@@ -55,69 +51,55 @@ const userController = {
             password: bcrypt.hashSync(req.body.password, 10),
             confirmPassword: bcrypt.hashSync(req.body.confirmPassword, 10),
             avatar: req.file.filename
-        };
+        }
         let userCreated = User.create(userCreate);
-        return res.redirect('/usuarios/ingresar');
-       /* const userCreate = req.body;
-        let ultimoUser = usuarios.pop();
-        usuarios.push(ultimoUser);
-        let userNuevo = {
-            id: ultimoUser.id +1,
-            nombre: userCreate.nombre,
-            apellido: userCreate.apellido,
-            email: userCreate.email,
-            password: bcrypt.hashSync(userCreate.password, 10),
-            confirmPassword: bcrypt.hashSync(userCreate.confirmPassword, 10),
-            avatar: req.file.filename
-        };
-        usuarios.push(userNuevo);
-        let userNuevoGuardar = JSON.stringify(usuarios,null,2);
-        fs.writeFileSync(path.resolve(__dirname,'../dataBase/users.json'), userNuevoGuardar);
-        res.redirect('/usuarios/ingresar');*/
+        if(userCreated) {
+            res.redirect('/usuarios/ingresar')
+        }
     },
     
     ingresar: (req, res) =>{
         res.render('users/login')
     },
     login: (req, res) =>{
-       const resultaValidation =  validationResult(req);
-        if (resultaValidation.errors.length > 0){
-            res.render('users/login', {
-                errors: resultaValidation.mapped(),
-                oldData: req.body,
-            });
-        };
-            let userLogin = User.findByField('email', req.body.email);
-
-            if(userLogin){
-                let passwordOk = bcrypt.compareSync(req.body.password, userLogin.password);
-                if(passwordOk){
-                    delete userLogin.password;
-                    delete userLogin.confirmPassword;
-                    req.session.userLogged = userLogin;
-                    if(req.body.recordar){
-                        res.cookie('userEmail', req.body.email, { maxAge: 1000 * 120})
-                    };
-                    return res.redirect('/usuarios/perfil');
-                }
-                return res.render('users/login', {
-                    errors: {
-                        email: {
-                            msg: 'Crendenciales inválidas'
-                        }
-                    },
-                    oldData: req.body,
-                });
-            }
-            return res.render('users/login', {
-                errors: {
-                    email: {
-                        msg: 'Este email no se encuentra registrado'
-                    }
-                },
-                oldData: req.body,
-            });
-    },
+        const resultaValidation =  validationResult(req);
+         if (resultaValidation.errors.length > 0){
+             res.render('users/login', {
+                 errors: resultaValidation.mapped(),
+                 oldData: req.body,
+             });
+         };
+             let userLogin = User.findByField('email', req.body.email);
+ 
+             if(userLogin){
+                 let passwordOk = bcrypt.compareSync(req.body.password, userLogin.password);
+                 if(passwordOk){
+                     delete userLogin.password;
+                     delete userLogin.confirmPassword;
+                     req.session.userLogged = userLogin;
+                     if(req.body.recordar){
+                         res.cookie('userEmail', req.body.email, { maxAge: 1000 * 120})
+                     };
+                     return res.redirect('/usuarios/perfil');
+                 }
+                 return res.render('users/login', {
+                     errors: {
+                         email: {
+                             msg: 'Crendenciales inválidas'
+                         }
+                     },
+                     oldData: req.body,
+                 });
+             }
+             return res.render('users/login', {
+                 errors: {
+                     email: {
+                         msg: 'Este email no se encuentra registrado'
+                     }
+                 },
+                 oldData: req.body,
+             });
+     },
     loguot: (req, res) => {
         res.clearCookie('userEmail');
         req.session.destroy();
